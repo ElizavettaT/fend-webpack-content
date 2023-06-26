f1cab82ba5b8bcb5e5126d5af22dc3873b55afd9
ElizavettaT <es.tolstikova@gmail.com>
2023-06-26 21:57:02 +0200

    Webpack entry

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,14 @@
 const path = require("path")
 const webpack = require("webpack")
 module.exports = {
-  entry: './src/client/js/index.js'
+  entry: './src/client/js/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  }
 }
